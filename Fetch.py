8536237ace93e881c284028f3935f02e1b3c3f9e
rasikaburde10 <42339068+rasikaburde10@users.noreply.github.com>
2024-07-19 14:02:36 +0100

    Update Fetch.py

--- Fetch.py
+++ Fetch.py
@@ -15,7 +15,7 @@ def save_to_file(data, filename):
     print(f"Data saved to {filename}")
 
 
-URL = 'https://hs-recruiting-test-resume-data.s3.amazonaws.com/allcands-full-api_hub_b1f6-acde48001122.json'  # Replace with the actual URL
+URL = 'https://hs-recruiting-test-resume-data.s3.amazonaws.com/allcands-full-api_hub_b1f6-acde48001122.json'  # given URL 
 FILENAME = 'fetched_data.json'  # Output file name
 
 data = fetch_data(URL)
